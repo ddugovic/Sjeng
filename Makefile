4992ca6f8046faebf289307cd52dad087d401635
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-08-27 21:35:03 +0200

    Add custom Makefile

---
+++ Makefile
@@ -0,0 +1,11 @@
+OBJS = attacks.o book.o draw.o epd.o learn.o moves.o newbook.o probe.o \
+	rcfile.o see.o seval.o ttable.o blob2.o crazy.o ecache.o eval.o leval.o \
+	neval.o partner.o proof.o search.o segtb.o sjeng.o utils.o
+
+sjeng: .depend $(OBJS)
+	$(CC) -o $@ $(OBJS) $(LDFLAGS)
+
+.depend:
+	$(CC) $(DEPENDFLAGS) -MM $(OBJS:.o=.c) > $@
+
+-include .depend
---
+++ config.h
@@ -0,0 +1,48 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define as the return type of signal handlers (int or void).  */
+#define RETSIGTYPE void
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you can safely include both <sys/time.h> and <time.h>.  */
+#define TIME_WITH_SYS_TIME 1
+
+/* Define if you have the ftime function.  */
+#define HAVE_FTIME 1
+
+/* Define if you have the gettimeofday function.  */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define if you have the select function.  */
+#define HAVE_SELECT 1
+
+/* Define if you have the strstr function.  */
+#define HAVE_STRSTR 1
+
+/* Define if you have the <sys/time.h> header file.  */
+#define HAVE_SYS_TIME_H 1
+
+/* Define if you have the <sys/timeb.h> header file.  */
+#define HAVE_SYS_TIMEB_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Define if you have the gdbm library (-lgdbm).  */
+#define HAVE_LIBGDBM 1
+
+/* Define if you have the m library (-lm).  */
+#define HAVE_LIBM 1
+
+/* Name of package */
+#define PACKAGE "Sjeng-Free"
+
+/* Version number of package */
+#define VERSION "11.2"
+
